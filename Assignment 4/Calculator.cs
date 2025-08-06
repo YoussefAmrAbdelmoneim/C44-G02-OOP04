d355cf04b7aa99e8874281e96690b0799c32911f
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-06 16:17:28 +0300

    Q1

---
+++ Assignment 4/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_4
+{
+    internal class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Add(int x, int y,int z)
+        {
+            return x + y + z;
+        }
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            #region Q1
+            //Calculator c1 = new Calculator();
+            //c1.Add(1,2);
+            //c1.Add(1, 2, 3);
+            //c1.Add(1.5, 2.7);
+            #endregion
+            #endregion
         }
     }
 }

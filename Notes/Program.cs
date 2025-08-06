5bd35758947b976c6cd5bd9c028a50b0e5f09902
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-06 18:38:18 +0300

    PolyMorphism

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Notes
+﻿using System.Diagnostics.Metrics;
+
+namespace Notes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region PolyMorphism
+            //PolyMorphism
+            //Polymorphism is referred to as the third pillar of object-oriented programming,
+            //after encapsulation and inheritance. Polymorphism is a Greek word that means "many-shaped" ,
+            //It enables methods to have different behaviors depending on the object that invokes them.
+            #endregion
         }
     }
 }

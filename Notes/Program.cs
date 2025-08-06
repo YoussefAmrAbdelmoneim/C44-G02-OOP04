24df7d461266521da6d80f9d8b6436aedc73901e
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-06 18:40:19 +0300

    Binding

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -12,6 +12,12 @@ namespace Notes
             //after encapsulation and inheritance. Polymorphism is a Greek word that means "many-shaped" ,
             //It enables methods to have different behaviors depending on the object that invokes them.
             #endregion
+            #region Binding
+            //Binding
+            //Reference From Parent Refer To an Object From Child 
+            //Static Binding |  Compile-time Binding | Early Binding --> Resolved during compilation at Compilation Time . 
+            //Dynamic Binding |  Run-time Binding | Late Binding -->  Resolved during Run-Time . 
+            #endregion
         }
     }
 }

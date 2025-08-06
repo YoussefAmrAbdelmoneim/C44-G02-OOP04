39feeee268604d36af53b2c91784ee32cfe0ce3f
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-06 18:20:45 +0300

    Operator Overloading

--- Demo session 4/Program.cs
+++ Demo session 4/Program.cs
@@ -22,11 +22,27 @@
         #endregion
         static void Main(string[] args)
         {
-           // int a = 10, b = 20;
-           //int result= SumNumbers(a, b);
-           // result = SumNumbers(10,20,30);
-           // SumNumbers(1.2, 10);
-           // Console.WriteLine(result);
+            // int a = 10, b = 20;
+            //int result= SumNumbers(a, b);
+            // result = SumNumbers(10,20,30);
+            // SumNumbers(1.2, 10);
+            // Console.WriteLine(result);
+            //Complex c1 = new Complex() { real = 10, imaginary = 5 };
+            //Complex c2 = new Complex() { real = 6, imaginary = 2 };
+            //Complex? c3 = default;
+            //c3 = c1 + c2;
+            //Console.WriteLine($"c1: {c1}");
+            //Console.WriteLine($"c2: {c2}");
+            //Console.WriteLine("----------------");
+            //Console.WriteLine($"c3: {c3}");
+            //c3--;
+            //Console.WriteLine($"c3: {c3}");
+            //if (c1 >c2)
+            //    Console.WriteLine("c1>c2");
+            //else if (c1<c2)
+            //    Console.WriteLine("c2>c1");
+            //else
+            //    Console.WriteLine("c1==c2");
         }
     }
 }

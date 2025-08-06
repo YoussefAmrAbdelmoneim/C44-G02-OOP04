6261d466a227d3df2a951b8f38d96cc80340ed77
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-06 17:59:37 +0300

    Part02

---
+++ Assignment 4/Duration.cs
@@ -0,0 +1,99 @@
+﻿namespace Assignment_4
+{
+    internal class Duration
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+        public Duration(int hours, int minutes, int seconds)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+
+        public Duration(int seconds)
+        {
+            this.hours = seconds / 3600;
+            int remainder = seconds % 3600;
+            this.minutes = remainder / 60;
+            this.seconds = remainder % 60;
+        }
+        public int TotalSeconds()
+        {
+            return this.hours * 3600 + this.minutes * 60 + this.seconds;
+        }
+        public override string ToString()
+        {
+            string result = "";
+            if (hours > 0)
+                result += $"Hours: {hours}, ";
+            if (minutes > 0 || hours > 0)
+                result += $"Minutes :{minutes}, ";
+            result += $"Seconds :{seconds}";
+            return result.TrimEnd(' ', ',');
+        }
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public static Duration operator +(Duration a, Duration b)
+        {
+            return new Duration((a?.TotalSeconds() ?? 0) + (b?.TotalSeconds() ?? 0));
+        }
+        public static Duration operator +(Duration a, int seconds)
+        {
+            return new Duration((a?.TotalSeconds() ?? 0) + seconds);
+        }
+        public static Duration operator +(int seconds, Duration a)
+        {
+            return new Duration((a?.TotalSeconds() ?? 0) + seconds);
+        }
+
+        public static Duration operator -(Duration a, Duration b)
+        {
+            return new Duration((a?.TotalSeconds() ?? 0) - (b?.TotalSeconds() ?? 0));
+        }
+        public static bool operator >(Duration a, Duration b)
+        {
+            return a?.TotalSeconds() > b?.TotalSeconds();
+        }
+        public static bool operator <(Duration a, Duration b)
+        {
+            return a?.TotalSeconds() < b?.TotalSeconds();
+        }
+        public static bool operator >=(Duration a, Duration b)
+        {
+            return a?.TotalSeconds() >= b?.TotalSeconds();
+        }
+        public static bool operator <=(Duration a, Duration b)
+        {
+            return a?.TotalSeconds() <= b?.TotalSeconds();
+        }
+        public static Duration operator ++(Duration a)
+        {
+            return new Duration((a?.TotalSeconds() ?? 0) + 60);
+        }
+        public static Duration operator --(Duration a)
+        {
+            return new Duration((a?.TotalSeconds() ?? 0) - 60);
+        }
+        public static bool operator true(Duration a)
+        {
+            return a.TotalSeconds() > 0;
+        }
+
+        public static bool operator false(Duration a)
+        {
+            return a.TotalSeconds() <0;
+        }
+        public static explicit operator DateTime(Duration a)
+        {
+            return DateTime.MinValue.AddSeconds(a.TotalSeconds());
+        }
+    }
+}
--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -39,6 +39,52 @@
             ////Resolve at Compile based on reference type
             #endregion
             #endregion
+            #region Part02
+            //Duration D1 = new Duration(1, 10, 15);
+            //Console.WriteLine(D1.ToString());
+
+            //Duration D2 = new Duration(3600);
+            //Console.WriteLine(D2.ToString());
+
+            //Duration D3 = new Duration(7800);
+            //Console.WriteLine(D3.ToString());
+
+            //Duration D4 = new Duration(666);
+            //Console.WriteLine(D4.ToString());
+            //Console.WriteLine(D1+D4);
+
+            //D3 = D1 + D2;
+            //Console.WriteLine(D3);
+            //D3 = D1 + 7800;
+            //Console.WriteLine(D3);
+
+            //D3 = 666 + D3;
+            //Console.WriteLine(D3);
+
+            //D3 = ++D1;
+            //Console.WriteLine(D3);
+
+            //D3 = --D2;
+            //Console.WriteLine(D3);
+
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1);
+            //if (D1 > D2)
+            //Console.WriteLine("D1 is greater than D2");
+            //else
+            //Console.WriteLine("D2 is greater than D1");
+
+            //if (D1 <= D2)
+            //    Console.WriteLine("D1 is smaller than or equal to D2");
+            //else
+            //    Console.WriteLine("D2 is greater than D1");
+            //if (D1)
+            //    Console.WriteLine("True");
+            //else
+            //    Console.WriteLine("False");
+            //DateTime Obj = (DateTime)D1;
+            //Console.WriteLine(Obj);
+            #endregion
         }
     }
 }

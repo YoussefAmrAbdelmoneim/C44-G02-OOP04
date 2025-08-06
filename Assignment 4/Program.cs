65a3f87bad8e5db0b55a47eea60e218bddcdb802
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-06 16:29:07 +0300

    Q3

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -14,6 +14,16 @@
             #region Q2
             //Rectangle r1 = new Rectangle(2,4);
             #endregion
+            #region Q3
+            //Complex c1 = new Complex();
+            //c1.real = 1;
+            //c1.imaginary = 2;
+            //Complex c2 = new Complex();
+            //c2.real = 3;
+            //c2.imaginary = 4;
+            //Console.WriteLine(c1+c2);
+            //Console.WriteLine(c1-c2);
+            #endregion
             #endregion
         }
     }

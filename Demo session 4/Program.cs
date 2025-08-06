3f5b23f349a30add5eecc4aae9e07594bfec0b02
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-06 18:04:59 +0300

    Method OverLoading

--- Demo session 4/Program.cs
+++ Demo session 4/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        #region Method OverLoading
+        //public static int SumNumbers(int x,int y)
+        //{
+        //    return x + y;
+        //}
+        //public static double SumNumbers(double x, int y)
+        //{
+        //    return x + y;
+        //}
+        //public static int SumNumbers(int x, int y,int z)
+        //{
+        //    return x + y +z;
+        //}
+        //public static double SumNumbers(double x, double y)
+        //{
+        //    return x + y;
+        //}
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           // int a = 10, b = 20;
+           //int result= SumNumbers(a, b);
+           // result = SumNumbers(10,20,30);
+           // SumNumbers(1.2, 10);
+           // Console.WriteLine(result);
         }
     }
 }
